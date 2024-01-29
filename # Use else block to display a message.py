9b45789f4c1919045f16a92f0024d50b5be3b2dc
vinaykonka556 <153889765+vinaykonka556@users.noreply.github.com>
2024-01-29 12:35:07 +0530

    Add files via upload

---
+++ # Use else block to display a message.py
@@ -0,0 +1,6 @@
+# Use else block to display a message “Done” after successful execution of for loop
+numbers =[1,2,3,4,5,]
+for numbers in numbers :
+    print(numbers)
+else:
+    print("loop completed successful.done!")    
