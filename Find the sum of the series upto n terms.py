8b0483ab8cd4bdd438413e735a36705cfda1c3ff
vinaykonka556 <153889765+vinaykonka556@users.noreply.github.com>
2024-01-29 12:32:52 +0530

    Add files via upload

---
+++ Find the sum of the series upto n terms.py
@@ -0,0 +1,6 @@
+# Find the sum of the series upto n terms.
+a = int(input("enter the number:"))
+sum = 0 
+for i in rangr (1,(2*a)+1,2):
+    sum +=i
+    print(f"sum of the first {a} odd number is: {sum}")
