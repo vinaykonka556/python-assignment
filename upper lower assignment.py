367b037f39f4f5dc37be5b328136b8d1dd01235d
vinaykonka556 <153889765+vinaykonka556@users.noreply.github.com>
2024-02-05 09:58:14 +0530

    Add files via upload

---
+++ upper lower assignment.py
@@ -0,0 +1,25 @@
+#upper()
+
+a = "python is a easy to learn"
+print(a.upper()) # PYTHON IS A EASY TO LEARN
+
+#lower()
+
+a = "PYTHON IS A EASY TO LEARN"
+print(a.lower()) # python is a easy to learn
+
+#swapcase()
+
+a = "python is a easy to learn"
+print(a.swapcase()) #  PYTHON IS A EASY TO LEARN
+
+#title()
+
+a = "python is a easy to learn"
+print(a.title())  #   Python Is A Easy To Learn
+
+
+# capitalize()
+
+a = "python is a easy to learn"
+print(a.capitalize()) # Python is a easy to learn
