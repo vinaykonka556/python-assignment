4a94bff00616de6610d23b85c2358f758b4d1345
vinaykonka556 <153889765+vinaykonka556@users.noreply.github.com>
2024-02-05 09:58:55 +0530

    Add files via upload

---
+++ index rfind rindex.py
@@ -0,0 +1,34 @@
+# find()
+
+a = "python is a easy to learn"
+#s = a.find("easy")
+#print(s) #12
+
+a = "python is a easy to learn"
+#s = a.find("is", 4,10)
+#print(s) #7
+
+
+#index()
+
+a = "python is a easy to learn"
+s = a.index("a")
+print(s)
+
+
+a = [1,23,4,5,67,7]
+s = a.index(67)
+print(s)
+
+
+#rfind()
+
+
+a = "python is a easy to learn"
+s= a.rfind("e", 1, 18)
+print(s)
+
+
+a = "python is a easy to learn"
+s= a.rfind("e", 1, 18)
+print(s)
